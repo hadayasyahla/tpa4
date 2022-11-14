f879c25f9650dc1956f85f2099eee68f4ac7a6e2
hadayasyahla <hadayasyahla@gmail.com>
2022-11-14 23:45:53 +0700

    update

--- src/componens/Artikel.js
+++ src/componens/Artikel.js
@@ -10,10 +10,10 @@ function Artikel() {
         
         <div className='card'>
           <div className='card-body'>
+            <h2>Add Friends</h2>
             <img className='img-artikel'
               src="https://blush.design/api/download?shareUri=LSNdf1RLUgYOOW8Y&c=Bottom_0%7E393f82-0.1%7E89c5cc-0.2%7E393f82-0.3%7E393f82_Hair_0%7Ee8e1e1-0.1%7E181658-0.2%7Ee8e1e1-0.3%7Eb58143_Skin_0%7Eb28b67-0.1%7E915b3c-0.2%7E915b3c-0.3%7E57331f_Top_0%7Eff4133-0.1%7Effa434-0.2%7Eff4133-0.3%7Ea8e5ba&bg=c97fc2&w=800&h=800&fm=png"
               alt="new"/>      
-            <h2>Add Friends</h2>
             <p class="card-text">Menambah pertemanan adalah hal utama bagi setiap manusia. </p>
             <p>Pada saat pandemi yang pertamakali ada di tahun 2020 sebagai manusia normal kita dibatasi untuk bertemu seseorang satu samalain , dan hanya menggunakan media online baik berbelanja ataupun melakukan aktifitas lainnya.</p>
             <p></p>
@@ -21,28 +21,26 @@ function Artikel() {
         </div>
 
     <div className='card'>
-      <div className='card-body'>
-          <p class="card-text">Menambah pertemanan adalah hal utama bagi setiap manusia. </p>
-          <p>Pada saat pandemi yang pertamakali ada di tahun 2020 sebagai manusia normal kita dibatasi untuk bertemu seseorang satu samalain , dan hanya menggunakan media online baik berbelanja ataupun melakukan aktifitas lainnya.</p>
-          <p></p>
-          <h2>Stay At Home</h2>
+      <div className='card-body'> 
+      <h2>Stay At Home</h2>
           <img className='img-artikel'
           src="https://blush.design/api/download?shareUri=v1duqOMrjrfiXmjR&c=Bottom_0%7E2b44ff-0.1%7E393f82_Hair_0%7Eb58143-0.1%7E2c1b18_Skin_0%7E57331f-0.1%7Edcae92_Top_0%7Ef2f2f2-0.1%7Effa434&w=800&h=800&fm=png"
           alt="new"/>
+          <p class="card-text"> </p>
+          <p>Pada saat pandemi yang pertamakali ada di tahun 2020 sebagai manusia normal kita dibatasi untuk bertemu seseorang satu samalain , dan hanya menggunakan media online baik berbelanja ataupun melakukan aktifitas lainnya.</p>
+          <p></p>
         </div>
     </div>
 
     <div className='card'>
-      <div className='card-body'>
-          <p class="card-text">Pada masa pandemi kita diwajibkan untuk berada dirumah</p>
-          <p>menjauhi krumunan agar tidak terpapar covid-19</p>
-          <h2>Workout</h2>
+      <h2>Workout</h2>
           <img className='img-artikel'
           src="https://blush.design/api/download?shareUri=tW2VL0KKW--YqqnY&c=Bottom_0%7E89c5cc_Hair_0%7Eb58143_Skin_0%7E57331f_Top_0%7Ea8e5ba&w=800&h=800&fm=png"
           alt="new"/>
+      <div className='card-body'>
           <p class="card-text">Olahraga sangat penting untuk tubuh kita</p>
           <p>olahraga mempunyai banyak manfaat dan membuat badan kita sehat</p>
-          </div>
+      </div>
     </div>
 
     </div>
